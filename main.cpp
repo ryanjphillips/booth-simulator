f0ad434a55181b02888a7fd396dbd7961321bdab
Ryan Phillips <ryan.phillips.j@gmail.com>
2024-04-08 23:17:14 -0500

    Project Update: Finished for the most part. Might be some bugs.

--- main.cpp
+++ main.cpp
@@ -1,16 +1,11 @@
-// Includes:
-
-#include <iostream>   
-#include <algorithm>  // For std::reverse
-#include <string>     
-
+#include <bits/stdc++.h>
 using namespace std;
  
 // Function to add the contents of the multiplicand to the accumulator.
 //
 void add(int accumulator[], int x[], int multiplierLength)
 {
-    int i; 
+    int i = 0; 
     int c = 0;
      
     for (i = 0; i < multiplierLength; i++) {
@@ -39,7 +34,8 @@ void complement(int a[], int n)
         a[increment] = (a[increment] + 1) % 2;
     }
 
-    int i, c = 0;
+    int i = 0; 
+    int c = 0;
      
     for (i = n - 1; i >= 0 ; i--) {
          
@@ -60,8 +56,8 @@ void complement(int a[], int n)
 
 void logicalShiftLeft(int multiplicand[], int multiplicandLength) {
 
-    int temp;
-    int i;
+    int temp = 0;
+    int i = 0;
 
     // Loops through reversed adding 0 to the LSB.
     for (i = multiplicandLength; i > 0; i--) {
@@ -77,7 +73,9 @@ void logicalShiftLeft(int multiplicand[], int multiplicandLength) {
 
 void arithmeticRightShft(int accumulator[], int multiplier[], int& E, int multiplierLength)
 {
-    int temp, i;
+    int temp = 0; 
+    int i = 0;
+
     temp = accumulator[0];
     E = multiplier[0];
      
@@ -92,7 +90,7 @@ void arithmeticRightShft(int accumulator[], int multiplier[], int& E, int multip
 
 void display(int accumulator[], int multiplier[], int multiplierLength)
 {
-    int i;
+    int i = 0;
      
     // accumulatorcumulator content
     for (i = multiplierLength - 1; i >= 0; i--)
@@ -117,7 +115,7 @@ void boothAlgorithm(int multiplicand[], int multiplier[], int multiplicandComp[]
     cout << "q[n+1]\tE\tmultiplicand\tAC\tmultiplier\tsequenceCounter\n";
     cout << "\t\tinitial\t\t";
     display(accumulator, multiplier, multiplierLength);
-    cout << "\t\t\t" << sequenceCounter << "\n";
+    cout << "\t\t" << sequenceCounter << "\n";
 
     // Main Loop, based on the Sequence Counter and based off the multiplicand length.
     
@@ -130,7 +128,7 @@ void boothAlgorithm(int multiplicand[], int multiplier[], int multiplicandComp[]
             // 10
             if (temp == 0) {
                  
-                add(accumulator, multiplicandComp, multiplierLength);
+                add(accumulator, multiplicandComp, multiplicandLength);
                 subCount += 1;
                 cout << "\tSub Counter:" << subCount << "\t";
                  
@@ -143,7 +141,7 @@ void boothAlgorithm(int multiplicand[], int multiplier[], int multiplicandComp[]
             else if (temp == 1)
             {
                 // add multiplicand to accumulatorcumulator
-                add(accumulator, multiplicand, multiplierLength);
+                add(accumulator, multiplicand, multiplicandLength);
                 addCount += 1;
                 cout << "\tAdd Counter:" << addCount << "\t" ;
                  
@@ -157,16 +155,17 @@ void boothAlgorithm(int multiplicand[], int multiplier[], int multiplicandComp[]
         }
          
         // 00 No Action, Shift Right AND 11 No Action, Shift Right
-        else if (E - multiplier[0] == 0)
+        else if (E - multiplier[0] == 0) {
             cout << "\tRight Shift\t";
             arithmeticRightShft(accumulator, multiplier, E, multiplierLength);
+        }
         
         display(accumulator, multiplier, multiplierLength);
         cout << "\t";
          
         // Decrement counter
         sequenceCounter--;
-        cout << "\t\t" << sequenceCounter << "\n";
+        cout << "\t" << sequenceCounter << "\n";
     }
 }
 
@@ -178,12 +177,28 @@ void modifiedBoothAlgorithm(int multiplicand[], int multiplier[], int multiplica
  
     int E = 0; 
     int accumulator[multiplicandLength] = { 0 };
-    // There is half in Modified Booth's Algoithm.
+    accumulator[multiplicandLength] = 0;
     sequenceCounter /= 2;
 
+    // Get twice Multiplicand
+    int twiceMultiplicand[multiplicandLength] = { 0 };
+    for (int i = multiplicandLength - 1; i >= 0; i--) {
+        twiceMultiplicand[i] = multiplicand[i]; 
+    }
+
+    logicalShiftLeft(twiceMultiplicand, multiplicandLength);
+    
+    // Get Twice Mulitplicant of Comp
+    int twiceMultiplicandComp[multiplicandLength] = { 0 };
+    for (int i = multiplicandLength - 1; i >= 0; i--) {
+        twiceMultiplicandComp[i] = multiplicandComp[i]; 
+    }
+
+    logicalShiftLeft(twiceMultiplicandComp, multiplicandLength);
+
     cout << "Modified Booth's Algorithm\n";
-    cout << "q[n+1]\tE\tmultiplicand\t\tAC\tmultiplier\tsequenceCounter\n";
-    cout << "\t\tinitial\t\t\t";
+    cout << "q[n+1]\tE\tmultiplicand\tAC\tmultiplier\tsequenceCounter\n";
+    cout << "\t\tinitial\t\t";
     display(accumulator, multiplier, multiplierLength);
     cout << "\t\t" << sequenceCounter << "\n";
      
@@ -199,34 +214,32 @@ void modifiedBoothAlgorithm(int multiplicand[], int multiplier[], int multiplica
         if ((first == 1 && second == 0 && third == 1) || (first == 1 && second == 1 && third == 0)) {
                  
             subCounter++;
-            cout << "\tSub Counter:" << subCounter << "\t\t";
-            add(accumulator, multiplicandComp, multiplierLength);
+            cout << "\tSub Counter:" << subCounter << "\t";
+            add(accumulator, multiplicandComp, multiplicandLength);
              
             for (int i = multiplierLength - 1; i >= 0; i--)
                 cout << accumulator[i];
 
         } else if ((first == 0 && second == 0 && third == 1) || (first == 0 && second == 1 && third == 0)) {
-                add(accumulator, multiplicand, multiplierLength);
+                add(accumulator, multiplicand, multiplicandLength);
                 addCounter++;
-                cout << "\tAdd Counter:" << addCounter << "\t\t";
+                cout << "\tAdd Counter:" << addCounter << "\t";
                  
                 for (int i = multiplierLength - 1; i >= 0; i--)
                     cout << accumulator[i];
 
         } else if ((first == 0 && second == 1 && third == 1)) {
-                logicalShiftLeft(multiplicand, multiplicandLength);
-                add(accumulator, multiplicand, multiplierLength);
+                add(accumulator, twiceMultiplicand, multiplicandLength);
                 addCounter++;
-                cout << "\tAdd Counter:" << addCounter << "\t\t";
+                cout << "\tAdd Counter:" << addCounter << "\t";
                  
                 for (int i = multiplierLength - 1; i >= 0; i--)
                     cout << accumulator[i];
 
         } else if ((first == 1 && second == 0 && third == 0)) {
-                logicalShiftLeft(multiplicandComp, multiplicandLength);
-                add(accumulator, multiplicandComp, multiplierLength);
+                add(accumulator, twiceMultiplicandComp, multiplicandLength);
                 addCounter++;
-                cout << "\tAdd Counter:" << addCounter << "\t\t";
+                cout << "\tAdd Counter:" << addCounter << "\t";
                  
                 for (int i = multiplierLength - 1; i >= 0; i--)
                     cout << accumulator[i];
@@ -234,7 +247,7 @@ void modifiedBoothAlgorithm(int multiplicand[], int multiplier[], int multiplica
 
         // Always Shift Right after it's complete.
         cout << "\n\t";
-        cout << "\tRight Shift\t\t";
+        cout << "\tRight Shift\t";
         arithmeticRightShft(accumulator, multiplier, E, multiplierLength);
         arithmeticRightShft(accumulator, multiplier, E, multiplierLength);
 
@@ -272,15 +285,15 @@ void algorithmInit(string currentMultiplicand, string currentMultiplier, int alg
     // Set the lengths of the multiplier and multiplicand.
     // Calculate the Two's complement of the multiplicnad.
     
-    string stringMultiplicand = currentMultiplicand;
-    string stringMultiplier = currentMultiplier;
+    string stringMultiplicand = "001011";
+    string stringMultiplier = "011001";
     int multiplierLength = stringMultiplier.length();
     int multiplicandLength = stringMultiplicand.length();
     int addCount = 0;
     int subCount = 0;
 
     // Move strings into bit array.
-    
+
     int* multiplicand = new int[multiplicandLength];
     populateBinaryArray(multiplicand, multiplicandLength, stringMultiplicand);
 
@@ -288,7 +301,7 @@ void algorithmInit(string currentMultiplicand, string currentMultiplier, int alg
     populateBinaryArray(multiplier, multiplierLength, stringMultiplier);
 
     int multiplicandComp[multiplicandLength]; 
-    int sequenceCounter;
+    int sequenceCounter = 0;
     
     // Create additional array of multiplicand.
     
@@ -311,13 +324,17 @@ void algorithmInit(string currentMultiplicand, string currentMultiplier, int alg
     } else {
       modifiedBoothAlgorithm(multiplicand, multiplier, multiplicandComp, multiplierLength, multiplicandLength, sequenceCounter, addCount, subCount);
     }
+
+    // Memory Management
+    delete[] multiplicand;
+    delete[] multiplier;
 }
 // Main function.
 
 int main() {
 
-    algorithmInit("11101", "110111", 0);
+    algorithmInit("0", "0", 0);
     cout << "\n";
-    algorithmInit("11101", "110111", 1);
+    algorithmInit("0", "0", 1);
     return 0;
 }
